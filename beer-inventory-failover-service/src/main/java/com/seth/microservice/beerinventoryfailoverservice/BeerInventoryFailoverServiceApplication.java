6723a12b6d4cd241ed5cd8408f9b5117547b6e7b
hemantseth0210 <hemant.seth02@gmail.com>
2020-12-06 21:20:17 -0500

    Added the Failover service for Beer Inventory

---
+++ beer-inventory-failover-service/src/main/java/com/seth/microservice/beerinventoryfailoverservice/BeerInventoryFailoverServiceApplication.java
@@ -0,0 +1,13 @@
+package com.seth.microservice.beerinventoryfailoverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerInventoryFailoverServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerInventoryFailoverServiceApplication.class, args);
+    }
+
+}
