117d7ff105314bfe2d660a27ad88a20b812cadde
hemantseth0210 <hemant.seth02@gmail.com>
2020-12-06 22:56:25 -0500

    Initial Project

---
+++ brewery-config-server/src/main/java/com/seth/microservices/breweryconfigserver/BreweryConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.seth.microservices.breweryconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryConfigServerApplication.class, args);
+	}
+
+}
